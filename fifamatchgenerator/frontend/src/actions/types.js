24950b70a749dfafb2e35015bd973add432e53ad
AKP-13 <alexpeirson@hotmail.com>
2020-10-16 18:04:23 +0100

    Add auth reducer and, on cDM, check whether user is authenticated

--- fifamatchgenerator/frontend/src/actions/types.js
+++ fifamatchgenerator/frontend/src/actions/types.js
@@ -3,3 +3,6 @@ export const DELETE_RESULT = 'DELETE_RESULT';
 export const ADD_RESULT = 'ADD_RESULT';
 export const GET_ERRORS = 'GET_ERRORS';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
