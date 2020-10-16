3177179b27a7d230551783a6994fb0c17aade48a
AKP-13 <alexpeirson@hotmail.com>
2020-10-16 14:13:14 +0100

    Alert shows 'There is an error' when you incorrectly submit new result

--- fifamatchgenerator/frontend/src/components/layout/Alerts.js
+++ fifamatchgenerator/frontend/src/components/layout/Alerts.js
@@ -1,9 +1,18 @@
 import React, { Component, Fragment } from 'react';
 import { withAlert } from 'react-alert';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 export class Alerts extends Component {
-    componentDidMount() {
-        this.props.alert.show('It works!')
+    static propTypes = {
+        error: PropTypes.object.isRequired
+    }
+
+    componentDidUpdate(prevProps) {
+        const { error, alert } = this.props;
+        if(error !== prevProps.error){
+            alert.error('There is an error')
+        }
     }
 
     render() {
@@ -15,4 +24,8 @@ export class Alerts extends Component {
     }
 }
 
-export default withAlert()(Alerts)
+const mapStateToProps = state => ({
+    error: state.errors
+})
+
+export default connect(mapStateToProps)(withAlert()(Alerts))
