2541e60393f1ec8f0603cad38c7cc266a79603ef
AKP-13 <alexpeirson@hotmail.com>
2020-10-16 10:37:14 +0100

    Add provider to app and pass store as a prop

--- fifamatchgenerator/frontend/src/components/App.js
+++ fifamatchgenerator/frontend/src/components/App.js
@@ -1,19 +1,24 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from "./layout/Header"
-import Dashboard from './results/Dashboard'
+import Header from "./layout/Header";
+import Dashboard from './results/Dashboard';
+
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header />
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
 
-                <div className="container">
-                    <Dashboard />    
-                </div>
-            </Fragment>
+                    <div className="container">
+                        <Dashboard />    
+                    </div>
+                </Fragment>
+            </Provider>
         )
     }
 }
