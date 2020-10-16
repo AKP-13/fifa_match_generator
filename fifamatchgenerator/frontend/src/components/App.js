6685c733b5154917dce35adf8d2129821948fb3e
AKP-13 <alexpeirson@hotmail.com>
2020-10-16 17:32:32 +0100

    Add templates for Register and Login pages on frontend

--- fifamatchgenerator/frontend/src/components/App.js
+++ fifamatchgenerator/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -7,6 +8,9 @@ import AlertTemplate from 'react-alert-template-basic';
 import Header from "./layout/Header";
 import Dashboard from './results/Dashboard';
 import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+
 
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -22,13 +26,19 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
-                    <Fragment>
-                        <Header />
-                        <Alerts />
-                        <div className="container">
-                            <Dashboard />    
-                        </div>
-                    </Fragment>
+                    <Router>
+                        <Fragment>
+                            <Header />
+                            <Alerts />
+                            <div className="container">
+                                <Switch>
+                                    <Route exact path="/" component={Dashboard} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+                                </Switch>
+                            </div>
+                        </Fragment>
+                    </Router>
                 </AlertProvider>
             </Provider>
         )
