c7405ee0508a2ef4f04c9da8764297d0d0076a2e
AKP-13 <alexpeirson@hotmail.com>
2020-10-16 17:44:35 +0100

    Create PrivateRoute which redirects, if not authenticated, to login

--- fifamatchgenerator/frontend/src/components/App.js
+++ fifamatchgenerator/frontend/src/components/App.js
@@ -10,7 +10,7 @@ import Dashboard from './results/Dashboard';
 import Alerts from './layout/Alerts';
 import Login from './accounts/Login';
 import Register from './accounts/Register';
-
+import PrivateRoute from './common/PrivateRoute';
 
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -32,7 +32,7 @@ class App extends Component {
                             <Alerts />
                             <div className="container">
                                 <Switch>
-                                    <Route exact path="/" component={Dashboard} />
+                                    <PrivateRoute exact path="/" component={Dashboard} />
                                     <Route exact path="/register" component={Register} />
                                     <Route exact path="/login" component={Login} />
                                 </Switch>
--- fifamatchgenerator/frontend/src/reducers/index.js
+++ fifamatchgenerator/frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import results from './results';
 import errors from './errors';
 import messages from './messages';
+import auth from './auth';
 
 export default combineReducers({
     results,
     errors,
-    messages
+    messages,
+    auth
 });
