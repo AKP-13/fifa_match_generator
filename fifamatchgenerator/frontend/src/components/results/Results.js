3d8629e50d2cecf36245bdfa84b88fb2fd63ffee
AKP-13 <alexpeirson@hotmail.com>
2020-10-16 11:25:40 +0100

    Display results data on cDM in table

--- fifamatchgenerator/frontend/src/components/results/Results.js
+++ fifamatchgenerator/frontend/src/components/results/Results.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getResults } from '../../actions/results';
@@ -14,9 +14,39 @@ export class Results extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Results List</h1>
-            </div>
+            <Fragment>
+                <h2>Results</h2>
+
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Opponent Name</th>
+                            <th>My Team</th>
+                            <th>My Goals</th>
+                            <th>Opponent Goals</th>
+                            <th>Opponent Team</th>
+                            <th>Notes</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.props.results.map(result => (
+                            <tr key={result.id}>
+                                <td>{result.id}</td>
+                                <td>{result.opponentName}</td>
+                                <td>{result.myTeam}</td>
+                                <td>{result.myGoals}</td>
+                                <td>{result.opponentGoals}</td>
+                                <td>{result.opponentTeam}</td>
+                                <td>{result.notes}</td>
+                                <td><button className="btn btn-danger btn-sm">Delete</button></td>
+                            </tr>
+                        )) }
+                    </tbody>
+                </table>
+
+            </Fragment>
         )
     }
 }
