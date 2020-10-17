77262211af3ce20bb9de743f32a2c35af2e89056
AKP-13 <alexpeirson@hotmail.com>
2020-10-17 15:49:42 +0100

    Add alert message for incorrect credentials on failed login

--- fifamatchgenerator/frontend/src/components/layout/Alerts.js
+++ fifamatchgenerator/frontend/src/components/layout/Alerts.js
@@ -31,6 +31,9 @@ export class Alerts extends Component {
             if(error.msg.notes) {
                 alert.error(`Notes: ${error.msg.notes.join()}`)
             }
+            if(error.msg.non_field_errors) {
+                alert.error(error.msg.non_field_errors.join())
+            }
         }
 
         if(message !== prevProps.message) {
