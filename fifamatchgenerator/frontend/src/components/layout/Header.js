112950a7d18c82edd2ca16e21e07248ed093ca96
AKP-13 <alexpeirson@hotmail.com>
2020-10-17 15:54:48 +0100

    Add 'Welcome <username>' to navbar

--- fifamatchgenerator/frontend/src/components/layout/Header.js
+++ fifamatchgenerator/frontend/src/components/layout/Header.js
@@ -14,7 +14,12 @@ export class Header extends Component {
         const { isAuthenticated, user } = this.props.auth;
 
         const authLinks = (
-            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">   
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                <span className="navbar-text mr-3">
+                    <strong>
+                        { user ? `Welcome ${user.username}` : "" }
+                    </strong>
+                </span>
                 <li className="nav-item">
                     <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">
                         Logout
