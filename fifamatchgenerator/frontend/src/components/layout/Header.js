64ebbe9a8264100db8b48395f3eec0c46ca189cc
AKP-13 <alexpeirson@hotmail.com>
2020-10-17 15:15:53 +0100

    Dynamically render different nav links dependent on isAuthenticated value

--- fifamatchgenerator/frontend/src/components/layout/Header.js
+++ fifamatchgenerator/frontend/src/components/layout/Header.js
@@ -1,8 +1,37 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+export class Header extends Component {
+    static propTypes = {
+        auth: PropTypes.object.isRequired
+    }
+
     render() {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">   
+                <li className="nav-item">
+                    <button className="nav-link btn btn-info btn-sm text-light">
+                        Logout
+                    </button>
+                </li>
+            </ul>
+        );
+
+        const guestLinks = (
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li className="nav-item">
+                    <Link to="/register" className="nav-link">Register</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/login" className="nav-link">Login</Link>
+                </li>
+            </ul>
+        )
+
         return (
             <nav className="navbar navbar-expand-sm navbar-light bg-light">
                 <div className="container">
@@ -11,17 +40,15 @@ export default class Header extends Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                         <a className="navbar-brand" href="#">FIFA 21 Random Match Generator</a>
-                        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                            <li className="nav-item">
-                                <Link to="/register" className="nav-link">Register</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/login" className="nav-link">Login</Link>
-                            </li>
-                        </ul>
                     </div>
+                    { isAuthenticated ? authLinks : guestLinks}
                 </div>
             </nav>
         )
     }
 }
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(Header);
