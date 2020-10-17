09f75e42a1f6d6c6b9f6aa6785d0c2985fd598ae
AKP-13 <alexpeirson@hotmail.com>
2020-10-17 15:02:58 +0100

    Add frontend login functionality

--- fifamatchgenerator/frontend/src/actions/auth.js
+++ fifamatchgenerator/frontend/src/actions/auth.js
@@ -4,7 +4,9 @@ import { returnErrors } from './messages';
 import {
     USER_LOADING,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from './types';
 
 // Check token and load user
@@ -37,6 +39,32 @@ export const loadUser = () => (dispatch, getState) => {
         dispatch(returnErrors(err.response.data, err.response.status));
         dispatch({
             type: AUTH_ERROR
+        });
+    });
+}
+
+// Login user
+export const login = (username, password) => dispatch => {
+    // Headers
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    // Request body
+    const body = JSON.stringify({ username, password })
+
+    axios.post('api/auth/login', body, config)
+    .then(res => {
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+    }).catch(err => {
+        dispatch(returnErrors(err.response.data, err.response.status));
+        dispatch({
+            type: LOGIN_FAIL
         });
     });
 }
--- fifamatchgenerator/frontend/src/reducers/auth.js
+++ fifamatchgenerator/frontend/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from '../actions/types'
+import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types'
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -21,7 +21,16 @@ export default function(state = initialState, action) {
                 isLoading: false,
                 user: action.payload
             }
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', action.payload.token);
+            return {
+                ...state,
+                ...action.payload,
+                isAuthenticated: true,
+                isLoading: false
+            }
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return {
                 ...state,
