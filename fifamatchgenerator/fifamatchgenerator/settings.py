1510df8a9b6bff10fe77c4056559b0287efac9e9
AKP-13 <alexpeirson@hotmail.com>
2020-10-22 11:45:12 +0100

    Update settings.py

--- fifamatchgenerator/fifamatchgenerator/settings.py
+++ fifamatchgenerator/fifamatchgenerator/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['akp13.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'home/AKP13/fifa_match_generator/fifamatchgenerator/frontend/static'
