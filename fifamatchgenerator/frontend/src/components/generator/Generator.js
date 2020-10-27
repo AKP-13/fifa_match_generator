8cb52752865ac0070475df8fe60eaa6ba7615810
AKP-13 <alexpeirson@hotmail.com>
2020-10-27 15:43:49 +0000

    Conditionally render login to record results button dependent on auth, add responsiveness features

--- fifamatchgenerator/frontend/src/components/generator/Generator.js
+++ fifamatchgenerator/frontend/src/components/generator/Generator.js
@@ -5,23 +5,26 @@ import Player1 from './Player1';
 import Player2 from './Player2';
 import Filters from './Filters';
 import '../../../../styles/Generator.css';
+import { Link } from 'react-router-dom';
 
 export class Generator extends Component {
     static propTypes = {
-        isAuthenticated: PropTypes.bool
+        auth: PropTypes.object.isRequired
     }
 
     render(){
+
+        const { isAuthenticated } = this.props.auth;
+
         return (
             <Fragment>
                 <div className="jumbotron jumbotron-fluid" id="jumbotron">
                     <div className="container" id="jumbotronContainer">
-                        <h1 className="display-4" id="welcomeText">Welcome to the FIFA 21 Random Match Generator!</h1>
-                            <ol>
-                                <li>Randomise the nation</li>
-                                <li>Randomise the league</li>
-                                <li>Randomise the team</li>
-                            </ol>
+                        <h1 className="display-4" id="welcomeText">
+                            Welcome to the FIFA 21 Random Match Generator!
+                        </h1>
+
+                        <p id="disclaimer">This app is not affiliated with or sponsored by Electronic Arts Inc. or its licensors.</p>
                     </div>
                 </div>
 
@@ -29,6 +32,9 @@ export class Generator extends Component {
                     <Player1 id="player1Component" />
                     <Player2 id="player2Component" />
                     <Filters id="filtersComponent" />
+                    { isAuthenticated ? 
+                    <Link to="/login" id="loginLink" >Record your results!</Link> : 
+                    <Link to="/login" id="loginLink" >Login to record your results!</Link> }
                 </div>
             </Fragment>
         )
@@ -36,7 +42,7 @@ export class Generator extends Component {
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+    auth: state.auth
 });
 
 export default connect(mapStateToProps)(Generator);
