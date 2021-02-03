29fb57586f910772599d3eb602b9aa6120be6337
AKP-13 <alexpeirson@hotmail.com>
2021-02-03 11:41:32 +0000

    Add toggle button for add result form

--- fifamatchgenerator/frontend/src/components/results/Form.js
+++ fifamatchgenerator/frontend/src/components/results/Form.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { addResult } from "../../actions/results";
 
+import { Icon, InlineIcon } from "@iconify/react";
+import addAlt from "@iconify-icons/carbon/add-alt";
+import subtractAlt from "@iconify-icons/carbon/subtract-alt";
+
 import "../../../../styles/Form.css";
 
 export class Form extends Component {
@@ -13,16 +17,18 @@ export class Form extends Component {
         opponentGoals: "",
         opponentTeam: "",
         notes: "",
+        add: false,
     };
 
     static propTypes = {
         addResult: PropTypes.func.isRequired,
     };
 
-    onChange = (e) =>
+    onChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value,
         });
+    };
 
     onSubmit = (e) => {
         e.preventDefault();
@@ -53,6 +59,13 @@ export class Form extends Component {
             opponentGoals: "",
             opponentTeam: "",
             notes: "",
+            add: false,
+        });
+    };
+
+    handleClick = (e) => {
+        this.setState({
+            add: !this.state.add,
         });
     };
 
@@ -68,126 +81,150 @@ export class Form extends Component {
 
         return (
             <div className="card card-body mt-4 mb-4 loginRegisterCard">
-                <h2 className="loginRegTitle">Add Result</h2>
-                <form onSubmit={this.onSubmit} id="addResultForm">
-                    <div className="form-row" id="opponentNameFormRow">
-                        <div className="col col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="opponentName">
-                                    Opponent Name <span id="asterisk">*</span>
-                                </label>
-                                <input
-                                    className="form-control"
-                                    type="text"
-                                    name="opponentName"
-                                    id="opponentName"
-                                    onChange={this.onChange}
-                                    value={opponentName}
-                                    placeholder="Harvey"
-                                />
-                            </div>
-                        </div>
-                    </div>
+                <h2 className="loginRegTitle">
+                    Results{" "}
+                    <button
+                        className="btn btn-outline-primary"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseExample"
+                        aria-expanded="false"
+                        aria-controls="collapseExample"
+                        id="addResultButton"
+                        onClick={this.handleClick}
+                    >
+                        {this.state.add ? (
+                            <Icon icon={subtractAlt} />
+                        ) : (
+                            <Icon icon={addAlt} />
+                        )}
+                    </button>
+                </h2>
 
-                    <div className="form-row">
-                        <div className="col">
-                            <div className="form-group">
-                                <label htmlFor="myTeam">
-                                    My Team <span id="asterisk">*</span>
-                                </label>
-                                <input
-                                    className="form-control"
-                                    type="text"
-                                    name="myTeam"
-                                    id="myTeam"
-                                    onChange={this.onChange}
-                                    value={myTeam}
-                                    placeholder="Spurs"
-                                />
+                <div className="collapse" id="collapseExample">
+                    <form onSubmit={this.onSubmit} id="addResultForm">
+                        <div className="form-row" id="opponentNameFormRow">
+                            <div className="col col-md-6">
+                                <div className="form-group">
+                                    <label htmlFor="opponentName">
+                                        Opponent Name{" "}
+                                        <span id="asterisk">*</span>
+                                    </label>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="opponentName"
+                                        id="opponentName"
+                                        onChange={this.onChange}
+                                        value={opponentName}
+                                        placeholder="Harvey"
+                                    />
+                                </div>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className="form-group">
-                                <label htmlFor="opponentTeam">
-                                    Opponent Team <span id="asterisk">*</span>
-                                </label>
-                                <input
-                                    className="form-control"
-                                    type="text"
-                                    name="opponentTeam"
-                                    id="opponentTeam"
-                                    onChange={this.onChange}
-                                    value={opponentTeam}
-                                    placeholder="Arsenal"
-                                />
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="form-row">
-                        <div className="col">
-                            <div className="form-group">
-                                <label htmlFor="myGoals">
-                                    My Goals <span id="asterisk">*</span>
-                                </label>
-                                <input
-                                    className="form-control goals"
-                                    type="number"
-                                    min="0"
-                                    name="myGoals"
-                                    id="myGoals"
-                                    onChange={this.onChange}
-                                    value={myGoals}
-                                    placeholder="3"
-                                />
+                        <div className="form-row">
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="myTeam">
+                                        My Team <span id="asterisk">*</span>
+                                    </label>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="myTeam"
+                                        id="myTeam"
+                                        onChange={this.onChange}
+                                        value={myTeam}
+                                        placeholder="Spurs"
+                                    />
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="opponentTeam">
+                                        Opponent Team{" "}
+                                        <span id="asterisk">*</span>
+                                    </label>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="opponentTeam"
+                                        id="opponentTeam"
+                                        onChange={this.onChange}
+                                        value={opponentTeam}
+                                        placeholder="Arsenal"
+                                    />
+                                </div>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className="form-group">
-                                <label htmlFor="opponentGoals">
-                                    Opponent Goals <span id="asterisk">*</span>
-                                </label>
-                                <input
-                                    className="form-control goals"
-                                    type="number"
-                                    min="0"
-                                    name="opponentGoals"
-                                    id="opponentGoals"
-                                    onChange={this.onChange}
-                                    value={opponentGoals}
-                                    placeholder="0"
-                                />
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="form-row" id="notesFormRow">
-                        <div className="col col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="notes">Notes</label>
-                                <textarea
-                                    className="form-control"
-                                    type="text"
-                                    name="notes"
-                                    id="notes"
-                                    onChange={this.onChange}
-                                    value={notes}
-                                    placeholder="#COYS"
-                                />
+                        <div className="form-row">
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="myGoals">
+                                        My Goals <span id="asterisk">*</span>
+                                    </label>
+                                    <input
+                                        className="form-control goals"
+                                        type="number"
+                                        min="0"
+                                        name="myGoals"
+                                        id="myGoals"
+                                        onChange={this.onChange}
+                                        value={myGoals}
+                                        placeholder="3"
+                                    />
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="opponentGoals">
+                                        Opponent Goals{" "}
+                                        <span id="asterisk">*</span>
+                                    </label>
+                                    <input
+                                        className="form-control goals"
+                                        type="number"
+                                        min="0"
+                                        name="opponentGoals"
+                                        id="opponentGoals"
+                                        onChange={this.onChange}
+                                        value={opponentGoals}
+                                        placeholder="0"
+                                    />
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="form-group">
-                        <button
-                            type="submit"
-                            className="btn btn-primary"
-                            id="resultsFormSubmitButton"
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </form>
+                        <div className="form-row" id="notesFormRow">
+                            <div className="col col-md-6">
+                                <div className="form-group">
+                                    <label htmlFor="notes">Notes</label>
+                                    <textarea
+                                        className="form-control"
+                                        type="text"
+                                        name="notes"
+                                        id="notes"
+                                        onChange={this.onChange}
+                                        value={notes}
+                                        placeholder="#COYS"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <button
+                                type="submit"
+                                className="btn btn-primary"
+                                id="resultsFormSubmitButton"
+                            >
+                                Add result
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
